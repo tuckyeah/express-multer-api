5939aca7852410b6037844dba56bdf7eb1872022
tuckyeah <h.rosebrock@gmail.com>
2016-10-14 11:46:58 -0400

    Name file uploads using crypto module
    - Define wrapper functions for naming the file
    - Some notes / comments were removed when we copied code from the
    slack channel - refer to previous commit if you need them.

--- bin/s3-upload.js
+++ bin/s3-upload.js
@@ -1,38 +1,37 @@
 'use strict';
-// this has to come before everything else!
-require('dotenv').config();
-// this is the same as doing:
+
+// this has to come before anything else
+require('dotenv').config();-// this is the same as doing:
 //    const throwaway = require('dotenv');
 //    throwaway.config();
 // but we don't need to do this because we don't need this variable, we just
 // need to execute config
 
 const fs = require('fs');
+const crypto = require('crypto');
+
 const fileType = require('file-type');
-// this is how amazon refers to it, so that's why it's all caps
+// breaking convention here, because Amazon requires it
 const AWS = require('aws-sdk');
 
 const filename = process.argv[2] || '';
 
-
 const readFile = (filename) => {
   return new Promise((resolve, reject) => {
     fs.readFile(filename, (error, data) => {
+      // reject causes the promise to be 'fulfilled' so it will still exit the function
       if (error) {
-        // reject causes the promise to be 'fulfilled' so it will still exit the function
         reject(error);
       }
+
       resolve(data);
     });
   });
 };
 
 // return a default object in the case that fileType is given an unsupported
-// file type to read.
+// filetype to read
 const mimeType = (data) => {
-  // this runs fileType(data) and adds it to the object literal
-  // we set a default file type here, the most basic type of all files (binary)
-  // it will be overwritten with the fileType data if the file actually has data (isn't null)
   return Object.assign({
     ext: 'bin',
     mime: 'application/octet-stream',
@@ -47,26 +46,58 @@ const parseFile = (fileBuffer) => {
   return file;
 };
 
-// this is an instance of the S3 manager that will be authenticated
+const randomHexString = (length) => {
+  return new Promise((resolve, reject) => {
+    crypto.randomBytes(length, (error, buffer) => {
+      if (error) {
+        reject(error);
+      }
+
+      resolve(buffer.toString('hex'));
+    });
+  });
+};
+
+// mini promise chain inside here - so it returns a Promise
+// but also does its little promise resolve chain in here
+const nameFile = (file) => {
+  // this outer return is what gets us back to the original promise chain
+  return randomHexString(16)
+  .then((val) => {
+    // we have to return _inside_ the promise chain
+    // so it doesn't break
+    file.name = val;
+    return file;
+  });
+};
+
+const nameDirectory = (file) => {
+  // this doesn't have to return a promise because it's not nested
+  // inside another promise (like nameFile is)
+  file.dir = new Date().toISOString().split('T')[0];
+  return file;
+};
+
+-// this is an instance of the S3 upload manager that will be authenticated
 const s3 = new AWS.S3({
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY,
-    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   }
 });
 
 const upload = (file) => {
   const options = {
-    // get bucket name from AWS console
+    // get the bucket name from your AWS S3 console
     Bucket: 'tucker-rosebrock-wdi',
-    // attach fileBuffer as stream to send to Amazon
+    // attach the fileBuffer as a stream to send to S3
     Body: file.data,
     // allow anyone to access the URL of the uploaded file
     ACL: 'public-read',
-    // tell Amazon what the mime-type is
+    // tell S3 what the mime-type is
     ContentType: file.mime,
     // pick a filename for S3 to use for the upload
-    Key: `test/test.${file.ext}`
+    Key: `${file.dir}/${file.name}.${file.ext}`
   };
 
   return new Promise((resolve, reject) => {
@@ -81,12 +112,15 @@ const upload = (file) => {
 };
 
 const logMessage = (response) => {
+  // turn the pojo into a string so I can see it on the console
   console.log(`the response from AWS was ${JSON.stringify(response)}`);
 };
 
-// now we have to call our function
 readFile(filename)
-  .then(parseFile)
-  .then(upload)
-  .then(logMessage)
-  .catch(console.error);
+.then(parseFile)
+.then(nameFile)
+.then(nameDirectory)
+.then(upload)
+.then(logMessage)
+.catch(console.error)
+;
